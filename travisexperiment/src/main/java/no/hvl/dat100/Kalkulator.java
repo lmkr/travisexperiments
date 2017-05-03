c9cf7c7c08e9d25e98f39b99b373f03bc82220b1
Lars Kristensen <lmkr@hib.no>
2017-05-03 13:08:44 +0200

    Added code for maven travis example

---
+++ travisexperiment/src/main/java/no/hvl/dat100/Kalkulator.java
@@ -0,0 +1,21 @@
+package no.hvl.dat100;
+
+public class Kalkulator {
+
+	public static int add(int tall1, int tall2) {
+		return tall1 + tall2;
+	}
+
+	public static int sub(int tall1, int tall2) {
+		return tall1 - tall2;
+	}
+	
+	public static int mul(int tall1, int tall2) {
+		return tall1 * tall2;
+	}
+
+	public static int div(int tall1, int tall2) {
+		return tall1 / tall2;
+	}
+}
+
